bf4f30233a18e64d490fdaafd780528f8954095f
Pavlo <pavdemchnko@gmail.com>
2024-12-17 17:28:38 -0500

    initial commit

---
+++ main.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWITCH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            switch (a) 
+            {
+                case "+":
+                    Console.WriteLine("You enter +");
+                    break;
+
+                case "-":
+                    Console.WriteLine("You enter -");
+
+                    break;
+
+                default:
+
+                    Console.WriteLine("I dont know");
+
+                    break;
+             }
+        }
+    }
+}
